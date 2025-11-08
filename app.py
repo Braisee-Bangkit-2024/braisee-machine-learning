b7ba398421309a052152dc0607f3a72e65acd901
FadilSeputra <112694789+FadilSeputra@users.noreply.github.com>
2025-11-08 15:01:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import cv2
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
 from tempfile import NamedTemporaryFile
- 
+
 
 # Initialize FastAPI app
 app = FastAPI()
