b321331810a1574134d30601e2a0638f7910b672
FadilSeputra <112694789+FadilSeputra@users.noreply.github.com>
2025-11-14 01:47:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from tempfile import NamedTemporaryFile
   
 # Initialize FastAPI app
 app = FastAPI()
- 
+
 # Label dictionary
 label_dict = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8,
               'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16,
