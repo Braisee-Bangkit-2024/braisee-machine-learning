cc378f4c38fad2a9f2d7f9acf53446add55c7c37
FadilSeputra <112694789+FadilSeputra@users.noreply.github.com>
2025-11-14 01:49:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ label_dict = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i
               'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16,
               'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24,
               'z': 25, 'empty': 26}
- 
+  
 # Load the model
 model_path = "braille1.h5"  # Update with your model path
 model = load_model(model_path)
